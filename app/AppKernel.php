7949e1df5f0c30780f164ee428da7239a78aa5ae
Moises Gallego <moisesgallego@gmail.com>
2011-07-05 20:35:32 +0200

    updated vendors and app files #4

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,7 +21,7 @@ class AppKernel extends Kernel
 	    new FOS\UserBundle\FOSUserBundle(),
 	    new SFM\UserBundle\SFMUserBundle(),
 	    new Knplabs\Bundle\MenuBundle\KnplabsMenuBundle(),
-	    new Knplabs\Bundle\PaginatorBundle\KnplabsPaginatorBundle(),
+	    //	    new Knplabs\Bundle\PaginatorBundle\KnplabsPaginatorBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
