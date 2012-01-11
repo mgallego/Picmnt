c11d4605032ff43ea18eeb2999f609f25c84d7e2
mgallego <moisesgallego@gmail.com>
2012-01-11 21:07:04 +0100

    decoupled the routing from the Index Controller and clean the code

--- src/SFM/PicmntBundle/Controller/IndexController.php
+++ src/SFM/PicmntBundle/Controller/IndexController.php
@@ -3,59 +3,28 @@
 namespace SFM\PicmntBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
-use Knplabs\Bundle\MenuBundle\MenuItem;
-
-
 
 class IndexController extends Controller
 {
 
-
-    /**
-    * @Route("/", name="home")
-    * @Template()
-    */
     public function indexAction()
     {
-      
-
       if ($this->get('security.context')->isGranted('ROLE_USER')){
-
 	return $this->redirect($this->generateUrl('secure_home'));
-	
       }
       else{
-	
-	return array();
-
+	return $this->render('SFMPicmntBundle:Index:index.html.twig');
       }
-       
     }
 
-
-    /**
-     * @Route("/p", name="secure_home")
-     * @Template()
-     */
     public function indexSecureAction()
     {
       return $this->redirect($this->generateUrl('img_show', Array("option"=>"random")));
-      //return $this->redirect($this->generateUrl('home'));
-
     }
 
-
-    /**
-     * @Route("/lang/{_locale}", name="lang")
-     */
     public function langAction()
     {
 	return $this->redirect($this->generateUrl('home'));
     }
-
 
 }
