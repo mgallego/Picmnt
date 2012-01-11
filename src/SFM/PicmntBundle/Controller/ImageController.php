f289784df73c3816d786610b4accc496d3da9d35
mgallego <moisesgallego@gmail.com>
2012-01-11 21:46:26 +0100

    decoupled the routing from the Image:edit action and refactorize

--- src/SFM/PicmntBundle/Controller/ImageController.php
+++ src/SFM/PicmntBundle/Controller/ImageController.php
@@ -25,311 +25,294 @@ use Symfony\Component\HttpFoundation\Response;
 class ImageController extends Controller
 {
   
-  public function uploadAction()
-  {
+    public function uploadAction()
+    {
     
-    $user = $this->container->get('security.context')->getToken()->getUser();
-    $image = new Image();
-    $imageUp = new ImageUp();
+	$user = $this->container->get('security.context')->getToken()->getUser();
+	$image = new Image();
+	$imageUp = new ImageUp();
             
-    $form = $this->createForm(new ImageUpType(), $imageUp);
+	$form = $this->createForm(new ImageUpType(), $imageUp);
 
-    if ($this->get('request')->getMethod() == 'POST'){
+	if ($this->get('request')->getMethod() == 'POST'){
 
-      $form->bindRequest( $this->get('request') );
+	    $form->bindRequest( $this->get('request') );
 
-      if ($form->isValid()) {
+	    if ($form->isValid()) {
 
-	$uploadedFile = $form['dataFile']->getData();
-	$imageUtil = new ImageUtil();
-	$extension = $imageUtil->getExtension($uploadedFile->getMimeType());
-	$newFileName = $user->getId().'_'.date("ymdHis").'_'.rand(1,9999).$extension;
+		$uploadedFile = $form['dataFile']->getData();
+		$imageUtil = new ImageUtil();
+		$extension = $imageUtil->getExtension($uploadedFile->getMimeType());
+		$newFileName = $user->getId().'_'.date("ymdHis").'_'.rand(1,9999).$extension;
 	
-	$uploadedFile->move(
-	  $_SERVER['DOCUMENT_ROOT']."/uploads",
-	  $newFileName );
+		$uploadedFile->move(
+		    $_SERVER['DOCUMENT_ROOT']."/uploads",
+		    $newFileName );
 
-	$imageUtil->resizeImage('uploads/'.$newFileName, 800);
+		$imageUtil->resizeImage('uploads/'.$newFileName, 800);
 
-	$image->setUrl($newFileName);
-	$image->setVotes(0);
-	$image->setUser($user);
+		$image->setUrl($newFileName);
+		$image->setVotes(0);
+		$image->setUser($user);
 
-	$em = $this->get('doctrine')->getEntityManager();     
-	$em->persist($image);
-	$em->flush();
+		$em = $this->get('doctrine')->getEntityManager();     
+		$em->persist($image);
+		$em->flush();
 
-	return $this->redirect($this->generateUrl('img_edit', array("id_image" => $image->getIdImage())));
-      }
-    }
-    return $this->render('SFMPicmntBundle:Image:upload.html.twig', array('form' => $form->createView()));
-  }
-
-
-  /**
-   * @Route("/img/edit/{id_image}", name="img_edit")
-   * @Template()
-   */
-  public function editImageAction($id_image){
-
-    $image = new Image();
-
-    $em = $this->get('doctrine')->getEntityManager();     
-    
-    $image = $em->find('SFMPicmntBundle:Image',$id_image);
-
-
-    $user = $this->container->get('security.context')->getToken()->getUser();
-
-
-    if ($user->getId() != $image->getUser()->getId()) { //diferent user
-      return $this->redirect($this->generateUrl('img_show', array("option"=>"random", "idImage"=>$id_image) ));
-    }
-    else{
-
-      $form = $this->createForm(new ImageType(), $image);
-
-      $request = $this->get('request');
-      
-      if ($request->getMethod() == 'POST'){
-
-	$form->bindRequest($request);
-      
-
-	if ($form->isValid()) {
-
-	  print($image->getidImage());
-	  print($image->getTitle());
-	  print($image->getDescription());
-
-	  $em->persist($image);
-	  $em->flush();
-	
-	  return $this->redirect($this->generateUrl('img_show', array("option"=>"random", "idImage"=>$image->getIdImage()) ));
-
+		return $this->redirect($this->generateUrl('img_edit', array("id_image" => $image->getIdImage())));
+	    }
 	}
-	else
-	  {
-
-	    return array("image_url" => 'uploads/'.$image->getUrl(), 'form' => $form->createView(), 'image'=>$image);
-	  }
-      }
-
-      return array("image_url" => 'uploads/'.$image->getUrl(), 'form' => $form->createView(), 'image'=>$image);
+	return $this->render('SFMPicmntBundle:Image:upload.html.twig', array('form' => $form->createView()));
     }
-  }
 
 
-  private function getRandomImage()
-  {
 
-    $em = $this->get('doctrine')->getEntityManager();
+
+    public function editAction($id_image){
+	$image = new Image();
+	$em = $this->get('doctrine')->getEntityManager();     
+	$image = $em->find('SFMPicmntBundle:Image',$id_image);
+
+	$user = $this->container->get('security.context')->getToken()->getUser();
+
+	if ($user->getId() != $image->getUser()->getId()) { //diferent user
+	    return $this->redirect($this->generateUrl('img_show', array("option"=>"random", "idImage"=>$id_image) ));
+	}
+	else{
+	    $form = $this->createForm(new ImageType(), $image);
+	    $request = $this->get('request');
+      
+	    if ($request->getMethod() == 'POST'){
+		$form->bindRequest($request);
+
+		if ($form->isValid()) {
+
+		    $em->persist($image);
+		    $em->flush();
+	
+		    return $this->redirect($this->generateUrl('img_show', array("option"=>"random", "idImage"=>$image->getIdImage()) ));
+		}
+		else{
+		    return $this->render('SFMPicmntBundle:Image:editImage.html.twig', array("image_url" => 'uploads/'.$image->getUrl(), 'form' => $form->createView(), 'image'=>$image));
+		}
+	    }
+	    return $this->render('SFMPicmntBundle:Image:editImage.html.twig', array("image_url" => 'uploads/'.$image->getUrl(), 'form' => $form->createView(), 'image'=>$image));
+	}
+    }
+
+
+
+
+    private function getRandomImage()
+    {
+
+	$em = $this->get('doctrine')->getEntityManager();
         	
-    $images = $em->getRepository('SFMPicmntBundle:Image')->getRandom();
+	$images = $em->getRepository('SFMPicmntBundle:Image')->getRandom();
 
-    if (!$images){
-	$e = $this->get('translator')->trans('There are no pictures in the database');
-	throw $this->createNotFoundException($e);
-    }
+	if (!$images){
+	    $e = $this->get('translator')->trans('There are no pictures in the database');
+	    throw $this->createNotFoundException($e);
+	}
 	    
           
-    $image = $images[0];
+	$image = $images[0];
 
-    return $image;
+	return $image;
     
-  }
+    }
  
 
 
-  //delete this function when finish the image show action 
-  /**
-   * @Route("/img/showTemp/{selection}", name="img_showTemp")
-   * //@Template()
-   */
-  public function getImageTempAction($selection){
+    //delete this function when finish the image show action 
+    /**
+     * @Route("/img/showTemp/{selection}", name="img_showTemp")
+     * //@Template()
+     */
+    public function getImageTempAction($selection){
 
-    $userVote = new UserVote();
+	$userVote = new UserVote();
 
-    $em = $this->get('doctrine')->getEntityManager();
+	$em = $this->get('doctrine')->getEntityManager();
     
-    //$em = $this->get('doctrine.orm.entity_manager');
+	//$em = $this->get('doctrine.orm.entity_manager');
     
 
-    if ($selection == 'last') {
-      $dql = "SELECT a FROM SFMPicmntBundle:Image a order by a.idImage desc";
+	if ($selection == 'last') {
+	    $dql = "SELECT a FROM SFMPicmntBundle:Image a order by a.idImage desc";
+	}
+
+	$query = $em->createQuery($dql);
+
+	$adapter = $this->get('knplabs_paginator.adapter');
+	$adapter->setQuery($query);
+	$adapter->setDistinct(true);
+    
+
+	$paginator = new \Zend\Paginator\Paginator($adapter);
+	$paginator->setCurrentPageNumber($this->get('request')->query->get('page', 1));
+	$paginator->setItemCountPerPage(1);
+	$paginator->setPageRange(1);
+
+	$items = $paginator->getCurrentItems();
+
+	$image = new Image();
+
+	//return the current object of the paginator
+	$image = $items[0];
+    
+	//parameters for the Javascript votef button in the layout
+	$parameters = Array("idImage"=>$image->getIdImage(), "voted"=>$this->hasVoted($image->getIdImage())
+		      , "page"=>$paginator->getCurrentPageNumber(), "selection"=>$selection);
+
+	$imageComment = new ImageComment();
+
+	//comments form
+	$formComment = $this->get('form.factory')
+	    ->createBuilder('form', $imageComment)
+	    ->add('comment', 'text')
+	    ->getForm();
+
+	//test
+	$imageNext = $em->getRepository('SFMPicmntBundle:Image')->findNext($image->getIdImage(),'p.idImage DESC');
+    
+	print($imageNext[0]->getIdImage(). ' actual '. $image->getIdImage());
+    
+
+	return array("paginator"=>$paginator, "parameters"=>$parameters, 
+	    "comments"=>$image->getImageComments(), "formComment"=>$formComment->createView());
     }
 
-    $query = $em->createQuery($dql);
 
-    $adapter = $this->get('knplabs_paginator.adapter');
-    $adapter->setQuery($query);
-    $adapter->setDistinct(true);
+
+    /**
+     * @Route("/img/{option}/{idImage}", defaults={"idImage"="0"}, name="img_show")
+     * @Template()
+     */
+    public function showImageAction($option, $idImage = 0){
     
+	$em = $this->get('doctrine')->getEntityManager();
+	$paginator = Array();
 
-    $paginator = new \Zend\Paginator\Paginator($adapter);
-    $paginator->setCurrentPageNumber($this->get('request')->query->get('page', 1));
-    $paginator->setItemCountPerPage(1);
-    $paginator->setPageRange(1);
+	if ( $option == 'last' ) {
 
-    $items = $paginator->getCurrentItems();
+	    if (!$image = $em->find('SFMPicmntBundle:Image',$idImage)){
 
-    $image = new Image();
+		$images = $em->getRepository('SFMPicmntBundle:Image')->findFirst('p.idImage DESC');
+		$image = $images[0];
 
-    //return the current object of the paginator
-    $image = $items[0];
+	    }
+
+	    $paginator = $this->getPaginator($option, $image->getIdImage());
+
+	}
+	else if ( $option = 'random' ){
+
+	    $image = $this->getRandomImage();
+
+	}
+      
+	return Array("image"=>$image, "paginator"=>$paginator);
+
+    }
+
+
+    private function getPaginator($option, $idImage){
     
-    //parameters for the Javascript votef button in the layout
-    $parameters = Array("idImage"=>$image->getIdImage(), "voted"=>$this->hasVoted($image->getIdImage())
-		  , "page"=>$paginator->getCurrentPageNumber(), "selection"=>$selection);
+	return Array('imgNext'=>$this->getNext($option, $idImage), 
+	    'imgPrevious'=>$this->getPrevious($option, $idImage) );
 
-    $imageComment = new ImageComment();
+    }
 
-    //comments form
-    $formComment = $this->get('form.factory')
-      ->createBuilder('form', $imageComment)
-      ->add('comment', 'text')
-      ->getForm();
 
-    //test
-    $imageNext = $em->getRepository('SFMPicmntBundle:Image')->findNext($image->getIdImage(),'p.idImage DESC');
+    private function getNext($orderOption, $idImage){
+
+	if ($orderOption == 'last'){
+
+	    $em = $this->get('doctrine')->getEntityManager();
     
-    print($imageNext[0]->getIdImage(). ' actual '. $image->getIdImage());
-    
+	    if ( ! $images = $em->getRepository('SFMPicmntBundle:Image')->findNext($idImage, 'p.idImage')){
 
-    return array("paginator"=>$paginator, "parameters"=>$parameters, 
-      "comments"=>$image->getImageComments(), "formComment"=>$formComment->createView());
-  }
+		$images = $em->getRepository('SFMPicmntBundle:Image')->findFirst('p.idImage DESC');    
 
+	    }
 
+	}
 
-  /**
-   * @Route("/img/{option}/{idImage}", defaults={"idImage"="0"}, name="img_show")
-   * @Template()
-   */
-  public function showImageAction($option, $idImage = 0){
-    
-    $em = $this->get('doctrine')->getEntityManager();
-    $paginator = Array();
-
-    if ( $option == 'last' ) {
-
-      if (!$image = $em->find('SFMPicmntBundle:Image',$idImage)){
-
-	$images = $em->getRepository('SFMPicmntBundle:Image')->findFirst('p.idImage DESC');
 	$image = $images[0];
 
-      }
-
-      $paginator = $this->getPaginator($option, $image->getIdImage());
+	return $image->getIdImage();
 
     }
-    else if ( $option = 'random' ){
 
-      $image = $this->getRandomImage();
+    private function getPrevious($orderOption, $idImage){
 
-    }
+	$em = $this->get('doctrine')->getEntityManager();
+
+	if ( $orderOption == 'last' ){
       
-    return Array("image"=>$image, "paginator"=>$paginator);
+	    if ( ! $images = $em->getRepository('SFMPicmntBundle:Image')->findPrevious($idImage, 'p.idImage DESC')){
 
-  }
+		$images = $em->getRepository('SFMPicmntBundle:Image')->findFirst('p.idImage DESC');    
 
-
-  private function getPaginator($option, $idImage){
-    
-    return Array('imgNext'=>$this->getNext($option, $idImage), 
-      'imgPrevious'=>$this->getPrevious($option, $idImage) );
-
-  }
-
-
-  private function getNext($orderOption, $idImage){
-
-    if ($orderOption == 'last'){
-
-      $em = $this->get('doctrine')->getEntityManager();
-    
-      if ( ! $images = $em->getRepository('SFMPicmntBundle:Image')->findNext($idImage, 'p.idImage')){
-
-	$images = $em->getRepository('SFMPicmntBundle:Image')->findFirst('p.idImage DESC');    
-
-      }
-
-    }
-
-    $image = $images[0];
-
-    return $image->getIdImage();
-
-  }
-
-  private function getPrevious($orderOption, $idImage){
-
-    $em = $this->get('doctrine')->getEntityManager();
-
-    if ( $orderOption == 'last' ){
-      
-      if ( ! $images = $em->getRepository('SFMPicmntBundle:Image')->findPrevious($idImage, 'p.idImage DESC')){
-
-	$images = $em->getRepository('SFMPicmntBundle:Image')->findFirst('p.idImage DESC');    
-
-      }
+	    }
      
+	}
+
+	$image = $images[0];
+
+	return $image->getIdImage();
+
     }
 
-    $image = $images[0];
-
-    return $image->getIdImage();
-
-  }
-
     
-  public function hasVoted($idImage){
+    public function hasVoted($idImage){
     
-    $user = $this->container->get('security.context')->getToken()->getUser();
+	$user = $this->container->get('security.context')->getToken()->getUser();
 
-    $repository = $this->get('doctrine')
-      ->getEntityManager()
-      ->getRepository('SFMPicmntBundle:UserVote');
+	$repository = $this->get('doctrine')
+	    ->getEntityManager()
+	    ->getRepository('SFMPicmntBundle:UserVote');
 
-    $query = $repository->createQueryBuilder('uv')
-      ->where('uv.userId = :userId AND uv.idImage = :idImage')
-      ->setParameters(array('userId'=>$user->getId(), 'idImage'=>$idImage))
-      ->getQuery();
+	$query = $repository->createQueryBuilder('uv')
+	    ->where('uv.userId = :userId AND uv.idImage = :idImage')
+	    ->setParameters(array('userId'=>$user->getId(), 'idImage'=>$idImage))
+	    ->getQuery();
 
-    $userVote = $query->getResult();
+	$userVote = $query->getResult();
     
     
-    if (!$userVote) { 
-      return 0;
-    }
-    else{
-      return 1;
+	if (!$userVote) { 
+	    return 0;
+	}
+	else{
+	    return 1;
+	}
+
     }
 
-  }
+    /**
+     * @Route("/img/vote/{idImage}", name="img_vote")
+     */
+    public function voteAction($idImage){
 
-  /**
-   * @Route("/img/vote/{idImage}", name="img_vote")
-   */
-  public function voteAction($idImage){
+	if ($this->hasVoted($idImage) == 0){
 
-    if ($this->hasVoted($idImage) == 0){
-
-      $em = $this->get('doctrine')->getEntityManager();
+	    $em = $this->get('doctrine')->getEntityManager();
       
-      $image = $em->find('SFMPicmntBundle:Image',$idImage);
+	    $image = $em->find('SFMPicmntBundle:Image',$idImage);
       
-      $image->sumVotes();
+	    $image->sumVotes();
       
-      $user = $this->container->get('security.context')->getToken()->getUser();
+	    $user = $this->container->get('security.context')->getToken()->getUser();
       
-      $image->addUserVotes($user);
+	    $image->addUserVotes($user);
     
-      $em->persist($image);
-      $em->flush();
+	    $em->persist($image);
+	    $em->flush();
       
-    }    
+	}    
 
-  }
+    }
 
 }
