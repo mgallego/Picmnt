4490073ac86680ef29ff944ae5ff50b757d2c428
mgallego <moisesgallego@gmail.com>
2012-02-03 14:31:28 +0100

    added translations and change the default lang

--- src/SFM/PicmntBundle/Controller/IndexController.php
+++ src/SFM/PicmntBundle/Controller/IndexController.php
@@ -3,12 +3,20 @@ namespace SFM\PicmntBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use SFM\PicmntBundle\Entity\Image;
+use Symfony\Component\HttpFoundation\Session;
 
 class IndexController extends Controller
 {
 
     public function indexAction()
     {
+      $locale = $this->get('session');
+      $request = $this->get('request');
+
+//      $lang = substr($request->server->get('HTTP_ACCEPT_LANGUAGE'),0,2);
+
+//      $request->setLocale($lang);
+
       if ($this->get('security.context')->isGranted('ROLE_USER')){
 	return $this->redirect($this->generateUrl('secure_home'));
       }
