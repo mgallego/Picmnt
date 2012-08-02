add178c08cd9ea68ad729524108b9f7e5be14f00
Moises Gallego <moisesgallego@gmail.com>
2012-08-02 10:57:14 +0200

    adapted form to SF2.1

--- src/SFM/PicmntBundle/Form/ImageUpType.php
+++ src/SFM/PicmntBundle/Form/ImageUpType.php
@@ -3,12 +3,12 @@
 namespace SFM\PicmntBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class ImageUpType extends AbstractType
 {
 
-  public function buildForm(FormBuilder $builder, array $options)
+  public function buildForm(FormBuilderInterface $builder, array $options)
   {
     $builder
 	->add('dataFile', 'file');
