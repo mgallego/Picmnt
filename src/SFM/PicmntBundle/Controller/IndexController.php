59ab6e35874030c17f9db969c01db23b217e3279
Moises Gallego <moisesgallego@gmail.com>
2012-08-06 15:42:03 +0200

    refactorize the IndexController. #29

--- src/SFM/PicmntBundle/Controller/IndexController.php
+++ src/SFM/PicmntBundle/Controller/IndexController.php
@@ -10,27 +10,20 @@ class IndexController extends Controller
 
     public function indexAction()
     {
-      $locale = $this->get('session');
-      $request = $this->get('request');
-            
+	$locale = $this->get('session');
+	$request = $this->get('request');
      
-      if ($this->get('security.context')->isGranted('ROLE_USER')){
-	return $this->redirect($this->generateUrl('secure_home'));
-      }
-      else{
-
-	  $em = $this->get('doctrine')->getEntityManager();
-
-	  $lastImages = $em->getRepository('SFMPicmntBundle:Image')->getLastImages(10);
-
-	  $mostCommentImages = $em->getRepository('SFMPicmntBundle:Image')->getMostComment(10);
-
-	  //	  print (\Doctrine\Common\Util\Debug::dump($mostCommentImages));
-	  
-	  $response = $this->render('SFMPicmntBundle:Index:index.html.twig', array('lastImages'=>$lastImages, 'mostComments'=>$mostCommentImages));
-	  $response->setPublic();
-	  return $response;
-      }
+	if ($this->get('security.context')->isGranted('ROLE_USER')){
+	    $response = $this->redirect($this->generateUrl('secure_home'));
+	}else{
+	    $em = $this->get('doctrine')->getEntityManager();
+	    $lastImages = $em->getRepository('SFMPicmntBundle:Image')->getLastImages(10);
+	    $mostCommentImages = $em->getRepository('SFMPicmntBundle:Image')->getMostComment(10);
+	    $responseData = array('lastImages'=>$lastImages, 'mostComments'=>$mostCommentImages);
+	    $response = $this->render('SFMPicmntBundle:Index:index.html.twig', $responseData);
+	    $response->setPublic();
+	}
+	return $response;
     }
 
     public function indexSecureAction()
@@ -45,7 +38,7 @@ class IndexController extends Controller
     
     public function staticAction($page)
     {
-      return $this->render('SFMPicmntBundle:Static:'.$page.'.html.twig');
+	return $this->render('SFMPicmntBundle:Static:'.$page.'.html.twig');
     }
 
 }
