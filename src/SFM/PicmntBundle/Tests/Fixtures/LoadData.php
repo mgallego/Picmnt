1e10961511a2f8565935c908a6761ec658e4657a
mgallego <moisesgallego@gmail.com>
2012-11-02 12:37:20 +0100

    Created a default category in the datafixtures tests

--- src/SFM/PicmntBundle/Tests/Fixtures/LoadData.php
+++ src/SFM/PicmntBundle/Tests/Fixtures/LoadData.php
@@ -11,68 +11,64 @@ use SFM\PicmntBundle\Entity\User;
 use SFM\PicmntBundle\Entity\Image;
 use SFM\PicmntBundle\Entity\Category;
 
-
 class LoadData implements FixtureInterface, ContainerAwareInterface
 {
     protected $userManager;
- 
+
     public function setContainer(ContainerInterface $container = null)
     {
-	$this->userManager = $container->get('fos_user.user_manager');
+        $this->userManager = $container->get('fos_user.user_manager');
     }
- 
+
     public function load(ObjectManager $em)
     {
-	$user = $this->userManager->createUser();
-	$user->setUsername('userTest');
-	$user->setEmail('test@picmnt.com');
-	$user->setPlainPassword('passwordTest');
-	$user->setEnabled(true);
-	$this->userManager->updateUser($user);
-    
-	$category = new Category();
+        $user = $this->userManager->createUser();
+        $user->setUsername('userTest');
+        $user->setEmail('test@picmnt.com');
+        $user->setPlainPassword('passwordTest');
+        $user->setEnabled(true);
+        $this->userManager->updateUser($user);
 
-	$category->setName('others');
-	$category->setStatus('1');
+        $category = new Category();
 
-	$images = $this->loadImages();
-      
-	foreach ($images as $ref => $ImageData)
-	{
-	    $image = new Image();
+        $category->setName('others');
+        $category->setStatus('1');
 
-	    foreach ($ImageData as $property => $value)
-	    {
-		$image->{'set'.ucfirst($property)}($value);
-	    }
+        $images = $this->loadImages();
 
-	    $image->setUser($user);
+        foreach ($images as $ref => $ImageData) {
+            $image = new Image();
 
-	
-	    $em->persist($image);
-	}
-    
-	$em->flush();
+            foreach ($ImageData as $property => $value) {
+                $image->{'set'.ucfirst($property)}($value);
+            }
+
+            $image->setUser($user);
+            $em->persist($image);
+        }
+        $em->flush();
     }
 
+    private function loadImages()
+    {
+        $images = array();
 
+        $category = new Category();
 
+        $category->setName('others');
+        $category->setStatus('1');
 
-    private function loadImages(){
-	$images = array();
+        for ($i=1; $i<11; $i++) {
+            $images[] = array('idImage'    => $i,
+                        'url'         => 'testImage'.$i.'.img',
+                        'title'       => 'title_'.$i,
+                        'description' => 'description_'.$i,
+                        'category'    => $category,
+                        'tags'        => 'tags_'.$i,
+                        'status'      => '1',
+                        'votes'       => '0');
+        }
 
-	for ($i=1; $i<11; $i++){
-	    $images[] = array('idImage'    => $i,
-			'url'         => 'testImage'.$i.'.img',
-			'title'       => 'title_'.$i,
-			'description' => 'description_'.$i,
-			'category'    => $category,
-			'tags'        => 'tags_'.$i,
-			'status'      => '1',
-			'votes'       => '0');
-	}
-	
-	return $images;
+        return $images;
     }
-
 }
