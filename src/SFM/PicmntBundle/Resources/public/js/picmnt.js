4bb8eca97f62111c1c41048313d0dc0c171b6881
Moises Gallego <moisesgallego@gmail.com>
2013-07-18 21:16:58 +0200

    [#WEB-63] - Fix the load more images issue

--- src/SFM/PicmntBundle/Resources/public/js/picmnt.js
+++ src/SFM/PicmntBundle/Resources/public/js/picmnt.js
@@ -1,29 +1,28 @@
 function loadMoreThumbs() {
-
     $.ajax({
 	url: '/app_dev.php/ajax/images/get_more?option='+option+'&page=' +  ++page +'&category='+category+'&username='+ username,
 	async: false,
 	dataType: 'json'
     }).success(function(images) {
+	console.log(images);
 	for (i in images)
 	{
 	    image = images[i];
 
-	    htmlText = "<div class='thumbnail'>\
-                       <div class='img-thumb'>\
+	    htmlText = "<li class='span3'>\
+                       <div class='thumbnail'>\
                        <a href='"+  image['imageViewUrl'] +"'><img src='" +  image['url'] + "' alt='" + image["title"] + "'/></a>\
                        <div class='thumb-info'>\
-                       <span>" + image["title"] + "</span>\
+                       <span><a href='"+  image['imageViewUrl'] +"'><h5>" + image["title"] + "</h5></a></span>\
                        <p>\
                        " + image['authorLabel'] + ": <a href='"+ image['userUrl'] +"'>\
                        "+ image["username"] +"\
                        </a>\
                        </p>\
                        </div>\
-                       </div>\
-                       </div>";
+                       </li>";
 
-            $('.img-thumbs').append(htmlText);
+            $('.thumbnails').append(htmlText);
 
 	    //document.location.href = '#load-more-thumbs';
 	}
