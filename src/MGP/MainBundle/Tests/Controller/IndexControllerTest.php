d3d322d250b34aded9b28f16c1fa2b09377d3081
Moises Gallego <moisesgallego@gmail.com>
2013-07-30 19:00:44 +0200

    Add test to test the tests xD

---
+++ src/MGP/MainBundle/Tests/Controller/IndexControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SFM\PicmntBundle\Tests\Controller;
+
+use SFM\PicmntBundle\Tests\Util\SecureAccess;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+
+class IndexControllerTest extends WebTestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
